483ceeb725004f3bbc5473698493d73e56d28cd9
Denis Craig <admin@deniscraig.com>
2018-11-28 17:52:34 +0000

    Log out things from init.d script

--- init.d/sourceforts.sh
+++ init.d/sourceforts.sh
@@ -5,12 +5,11 @@ PIDFILE=/var/run/$NAME/$NAME.pid
 SCRIPTNAME=/etc/init.d/$NAME
 
 CHDIR=/home/steam
-DIR=/home/steam/steamcmd
-DAEMON="$CHDIR/srcds_run"
+DAEMON=srcds_run
 
 GAME="sourceforts"
 SF_ARGS="+sf_team_blocklimit $BLOCK_LIMIT +sf_build_long_length $BUILD_LENGTH_LONG +sf_combat_length $COMBAT_LENGTH"
-SRCDS_ARGS="+fps_max $SRCDS_FPSMAX -tickrate $SRCDS_TICKRATE -port $SRCDS_PORT -tv_port $SRCDS_TV_PORT +maxplayers $SRCDS_MAXPLAYERS +rcon_password $SRCDS_PW +sv_password $SRCDS_PW"
+SRCDS_ARGS="+fps_max $SRCDS_FPSMAX -tickrate $SRCDS_TICKRATE -port $SRCDS_PORT -tv_port $SRCDS_TV_PORT +maxplayers $SRCDS_MAXPLAYERS +rcon_password $SRCDS_RCONPW +sv_password $SRCDS_PW"
 EXTRA_ARGS="$SRCDS_ARGS $SF_ARGS"
 ARGS="-game $GAME -console -norestart +ip 0.0.0.0 +hostname $HOSTNAME +map $DEFAULT_MAP +exec $SOURCEFORTS_DIR/server.cfg $EXTRA_ARGS"
 
@@ -18,30 +17,33 @@ export MALLOC_CHECK_=0
 
 [ -x "$DAEMON" ] || exit 0
 
-DAEMON_ARGS="-steam_dir $DIR -steamcmd_script $DIR/steamcmd/steamcmd.sh -pidfile $PIDFILE $ARGS $CVARS"
+DAEMON_ARGS="-steam_dir $CHDIR -steamcmd_script $CHDIR/steamcmd/steamcmd.sh -pidfile $PIDFILE $ARGS"
 
 . /lib/init/vars.sh
-VERBOSE="yes"
 . /lib/lsb/init-functions
 
 do_start() {
     export MALLOC_CHECK_=0
+    touch $CHDIR/Steam/logs/daemon.log
     touch $PIDFILE
     chown steam:steam $PIDFILE
-    start-stop-daemon --start --quiet --pidfile $PIDFILE --chuid steam:steam --chdir $CHDIR --exec $DAEMON --test > /dev/null || return 1
-    start-stop-daemon --start --pidfile $PIDFILE --background --chuid steam --chdir $CHDIR --exec $DAEMON -- $DAEMON_ARGS || return 2
+    start-stop-daemon --start --quiet --pidfile $PIDFILE --chuid steam:steam --chdir $CHDIR --exec $DAEMON --test >> $CHDIR/Steam/logs/daemon.log 2>&1
+    [ "$?" != 0 ] && return 1
+    start-stop-daemon --start --pidfile $PIDFILE --background --chuid steam --chdir $CHDIR --exec $DAEMON -- $DAEMON_ARGS >> $CHDIR/Steam/logs/daemon.log 2>&1
+    [ "$?" != 0 ] && return 2
 }
 do_stop() {
-    start-stop-daemon --stop --quiet --retry=TERM/30/KILL/5 --pidfile $PIDFILE --user steam
+    start-stop-daemon --stop --quiet --retry=TERM/30/KILL/5 --pidfile $PIDFILE --user steam >> $CHDIR/Steam/logs/daemon.log 2>&1
     RETVAL="$?"
     [ "$RETVAL" = 2 ] && return 2
-    start-stop-daemon --stop --quiet --oknodo --retry=TERM/30/KILL/5 --exec $DAEMON
+    start-stop-daemon --stop --quiet --oknodo --retry=TERM/30/KILL/5 --exec $DAEMON >> $CHDIR/Steam/logs/daemon.log 2>&1
     [ "$?" = 2 ] && return 2
     rm -f $PIDFILE
     return "$RETVAL"
 }
 case "$1" in
     start)
+        echo "Starting $DESC: $NAME" >> $CHDIR/Steam/logs/daemon.log
         [ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC" "$NAME"
         do_start
         case "$?" in
@@ -50,6 +52,7 @@ case "$1" in
         esac
         ;;
     stop)
+        echo "Starting $DESC: $NAME" >> $CHDIR/Steam/logs/daemon.log
         [ "$VERBOSE" != no ] && log_daemon_msg "Stopping $DESC" "$NAME"
         do_stop
         case "$?" in
