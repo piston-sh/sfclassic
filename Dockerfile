86a5509d717b4372b6a2d32b365385f6c95f752b
Denis Craig <dcraig@thousandeyes.com>
2018-12-01 16:31:56 +0000

    Make the dockerfile a bit more generic

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,16 @@
 FROM sourceforts/srcds-server
 LABEL maintainer="admin@deniscraig.com"
 
+ARG version=1
+ENV VERSION $version
+
+ARG game=sourceforts
+ENV GAME=sourceforts
+ENV HOSTNAME="docker-$game"
+
+ARG default_map=sf_skywalk
+ENV DEFAULT_MAP=$default_map
+
 # <API setup>
 ENV API_DIR=$STEAM_USER_DIR/api
 USER root
@@ -12,24 +22,19 @@ COPY --chown=steam:steam api $API_DIR
 USER steam
 # </API setup>
 
-ARG version=1
-ENV VERSION $version
-
 # <Game setup>
-ENV SOURCEFORTS_DIR=$STEAM_USER_DIR/sourceforts
+ENV GAME_DIR=$STEAM_USER_DIR/$game
 
-COPY --chown=steam:steam sourceforts $SOURCEFORTS_DIR
+COPY --chown=steam:steam $game $GAME_DIR
 # Override server config
-COPY --chown=steam:steam cfg $SOURCEFORTS_DIR/cfg
+COPY --chown=steam:steam cfg $GAME_DIR/cfg
 
-ENV HOSTNAME="docker-sourceforts"
-ENV DEFAULT_MAP="sf_skywalk"
 ENV BLOCK_LIMIT=50 
 ENV BUILD_LENGTH_LONG=600 
 ENV BUILD_LENGTH_SHORT=240 
 ENV COMBAT_LENGTH=600
 
-VOLUME $SOURCEFORTS_DIR
+VOLUME $GAME_DIR
 # </Game setup>
 
 COPY --chown=steam:steam start.sh start.sh
--- start.sh
+++ start.sh
@@ -2,11 +2,10 @@
 
 LOG_DIR=$STEAM_USER_DIR/Steam/logs
 LOG_FILE=$LOG_DIR/daemon.log
-GAME="sourceforts"
 SF_ARGS="+sf_team_blocklimit $BLOCK_LIMIT +sf_build_long_length $BUILD_LENGTH_LONG +sf_combat_length $COMBAT_LENGTH"
 SRCDS_ARGS="+fps_max $SRCDS_FPSMAX -tickrate $SRCDS_TICKRATE -port $SRCDS_PORT -tv_port $SRCDS_TV_PORT +maxplayers $SRCDS_MAXPLAYERS +rcon_password $SRCDS_RCONPW +sv_password $SRCDS_PW"
 EXTRA_ARGS="$SRCDS_ARGS $SF_ARGS"
-ARGS="-game $GAME -console -norestart +ip 0.0.0.0 +hostname $HOSTNAME +map $DEFAULT_MAP +exec $SOURCEFORTS_DIR/server.cfg $EXTRA_ARGS"
+ARGS="-game $GAME -console -norestart +ip 0.0.0.0 +hostname $HOSTNAME +map $DEFAULT_MAP +exec $GAME_DIR/server.cfg $EXTRA_ARGS"
 
 if [ ! -f $LOG_FILE ]; then
     touch $LOG_FILE
@@ -16,7 +15,7 @@ fi
 $API_DIR/node_modules/.bin/pm2 start $API_DIR/index.js
 
 # Update hostnames
-find $SOURCEFORTS_DIR/cfg -name *.cfg -exec sed -i "s/{hostname}/$HOSTNAME/g" {} \;
+find $GAME_DIR/cfg -name *.cfg -exec sed -i "s/{hostname}/$HOSTNAME/g" {} \;
 
 ./srcds_run -steam_dir $(pwd) -steamcmd_script $(pwd)/steamcmd/steamcmd.sh $ARGS >> $LOG_FILE 2>&1
 tail -F $LOG_DIR/*
